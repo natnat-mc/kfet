d65d715ae4a7125dc8df283618406ee3044dad26
DECHER Nathan <nathan.decher@gmail.com>
2019-01-10 00:10:48 +0100

    index.js sets working directory, cleaner code

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // load config
 const config=require('./config');
-let env=Object.assign({}, config.env || {}, process.env);
+
+// load evironment variables
+let env=Object.assign({}, config.env, process.env);
 Object.assign(process.env, env);
+
+// set working directory to where the script is located
+process.chdir(__dirname);
 
 // load everything
 require('./src/express');
