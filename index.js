0296ffa27af4d0c6679eced3ba03973b95c4b6f6
DECHER Nathan <nathan.decher@gmail.com>
2019-01-12 00:17:01 +0100

    hopefully exits on signal

--- index.js
+++ index.js
@@ -24,3 +24,14 @@ require('./src/db');
 
 // load everything
 require('./src/express');
+
+// setup signal handlers to exit
+((a => {
+	[].forEach.call(['int', 'term', 'hup'], a);
+})(s => {
+	s='SIG'+s.toUpperCase();
+	process.on(s, () => {
+		log.notice("Exiting on "+s);
+		process.exit(0);
+	});
+}));
