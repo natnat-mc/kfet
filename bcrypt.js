9eff31aaccc44583411b9a24edc9dd63d4d917cb
Nathan DECHER <nathan.decher@gmail.com>
2019-01-13 22:16:31 +0100

    removed bcrypt.js which shouldn't have been there

--- bcrypt.js
+++
@@ -1,7 +0,0 @@
-const bcrypt=require('bcrypt');
-const str="azazazaza";
-const rounds=8;
-const salt=bcrypt.genSaltSync(rounds);
-const hash=bcrypt.hashSync(str, salt);
-const eq=bcrypt.compareSync(hash, hash);
-console.log(str, rounds, salt, hash, eq, hash.length, process.version);
