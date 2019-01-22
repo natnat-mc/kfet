263744ac05bbf6860cacb9f05a8786aa2c0bee78
Nathan DECHER <nathan.decher@gmail.com>
2019-01-22 12:53:20 +0100

    cleaned up dead code

--- src/express.js
+++ src/express.js
@@ -24,67 +24,6 @@ app.use('/views', express.static(process.cwd()+'/views')); // allow views to be 
 require('./api')(app);
 require('./webui')(app);
 
-/*
-// use authenticator
-app.use((req, res, next) => {
-	// check if we have a session cookie
-	if(req.cookies['session']) {
-		let user=auth.checkCookie(req.cookies['session']);
-		if(user) {
-			// if it's genuine, use it as our session
-			req.session=user;
-			log.debug('found valid cookie for user %s', user.username);
-		} else {
-			// if it isn't, delete it
-			res.cookie('session', '', {maxAge: -1000*60*60*24});
-			log.debug('found invalid cookie: %s', req.cookies['session']);
-		}
-	}
-	res.locals.session=req.session;
-	next();
-});
-app.post('/api/login', async (req, res) => {
-	// see what we got there
-	if(req.body.login && req.body.password) {
-		// attempt to authenticate someone
-		let user=await auth.authUser(req.body.login, req.body.password);
-		if(user) {
-			// save their session
-			req.session=user;
-			// give them a cookie
-			let cookie=await auth.createCookie(user);
-			res.cookie('session', cookie, req.body.rememberme?{maxAge: auth.duration}:undefined);
-			res.redirect('back');
-			log.info('User %s logged in', user.username);
-		} else {
-			//TODO render some kind of error page
-			log.info('Failed login attempt as %s', req.body.login);
-			return res.status(401).render('loginError', {
-				login: req.body.login
-			});
-		}
-	} else {
-		//TODO what the fuck did they even mean to do?
-		return res.status(400).render('genericError', {
-			short: "WTF",
-			message: "Why did you try to log in without any login/password combo?"
-		});
-	}
-});
-app.all('/api/logout', (req, res) => {
-	// delete the session cookie
-	res.cookie('session', '', {maxAge: -1000*60*60*24});
-	//TODO delete it from db too
-	// redirect them to where they came from
-	res.redirect('back');
-});
-
-//NOTICE this is debug only
-app.get('/', (req, res) => {
-	return res.status(200).render('loginBoxTest');
-});
-*/
-
 // listen on either the port given in env (or loaded from config) or 3000
 const server=app.listen(process.env.PORT || 3000, process.env.ADDRESS || '0.0.0.0', () => {
 	log.notice("Webserver listening on "+(process.env.ADDRESS || '0.0.0.0')+":"+(process.env.PORT || 3000));
